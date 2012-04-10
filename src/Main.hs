c7e6ceef704413bb228ddca82fb3811b4e628f11
Pochapskiy Olexandr <olexandr.pochapskiy@gmail.com>
2012-04-10 15:15:46 +0300

    Added basic argument handling

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,12 @@
 -----------------------------------------------------------------------------
 --
 -- Module      :  Main
--- Copyright   :  
+-- Copyright   :
 -- License     :  AllRightsReserved
 --
--- Maintainer  :  
--- Stability   :  
--- Portability :  
+-- Maintainer  :
+-- Stability   :
+-- Portability :
 --
 -- |
 --
@@ -16,5 +16,40 @@ module Main (
     main
 ) where
 
-main = putStrLn "Hello World!"
+import System
+import System (getArgs)
+import System.Console.GetOpt
 
+data Options = Options{
+    version :: IO (),
+    optOutput :: IO ()
+}
+
+defaultOptions :: Options
+defaultOptions = Options {
+    version = return(),
+    optOutput = return ()
+}
+
+options :: [OptDescr (Options -> IO Options)]
+options = [
+            Option ['V'] ["version"]    (NoArg (\opt -> return opt { version = showVersion })) "show version number",
+            Option ['L'] ["list"]       (NoArg (\opt -> return opt { optOutput = showSubscriptions}))  "show list of subscriptions"
+            ]
+
+header = "Usage: main [Option...]"
+
+showSubscriptions = do
+    putStrLn "Placeholder for showing feeds"
+    exitWith ExitSuccess
+
+showVersion = do
+    putStrLn "hrss version 0.0.1"
+    exitWith ExitSuccess
+
+main = do
+    args <- getArgs
+    let ( actions, nonOpts, msgs ) = getOpt RequireOrder options args
+    opts <- foldl (>>=) (return defaultOptions) actions
+    let Options {optOutput = outputFun, version = versionFun} = opts
+    outputFun >> versionFun
